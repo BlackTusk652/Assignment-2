4dd86e4461d6e23a94fd11433d699820bf24ce54
BlackTusk652 <anisha.navaratnam@alumni.ubc.ca>
2017-02-14 11:19:39 -0800

    Assignment Two Answers
    with updated comments

--- convAdvec.m
+++ convAdvec.m
@@ -31,37 +31,39 @@ A = diff + conv;
 p = A\q'
 
 
-%%Test Function of ConvAdvec to produce sin graph. experimental and
-%%theoretical both plotted at the end
-%% used different file to test the code above and it worked :D 
-for i = 2:4
-h=10.^(-i);
-n=1/h;
-% x on the nodes
-xN = [0:h:1];
-% x on the nodes excluding the end points
-xI = xN(2:n);
-% x on the cell centers
-xC = [(h/2):h:(1-(h/2))];
-mu = 0.1;
-%mu = 10;
-% creating sigma points
-sig = (1+xC.^2)';
-Sig = spdiags(sig, [0], n, n);
-% q flow 
-diff = 4*pi*xI.*cos(2*pi*xI)-4*pi^2*(xI.^2 + 1).*sin(2*pi*xI);
-flow = mu*2*pi*cos(2*pi*xI);
-q =  diff + flow;
-Pexp = convAdvec(h,Sig,mu,q);
-Ptheo = sin(2*pi*xC);
-i+1;
+% %%Test Function of ConvAdvec to produce sin graph. experimental and
+% %%theoretical both plotted at the end
+% %% used different file to test the code above and it worked :D 
 
-figure
-hold on
-plot(Pexp)
-plot(Ptheo)
-hold off
-pause(10);
-end
-%%this code stop running at h = 10^-5 and errors itself (ie when h is less
-%%than 10^-4
+% for i = 2:4
+% h=10.^(-i);
+% n=1/h;
+% % x on the nodes
+% xN = [0:h:1];
+% % x on the nodes excluding the end points
+% xI = xN(2:n);
+% % x on the cell centers
+% xC = [(h/2):h:(1-(h/2))];
+% mu = 0.1;
+% %mu = 10;
+% % creating sigma points
+% sig = (1+xC.^2)';
+% Sig = spdiags(sig, [0], n, n);
+% % q flow 
+% diff = 4*pi*xI.*cos(2*pi*xI)-4*pi^2*(xI.^2 + 1).*sin(2*pi*xI);
+% flow = mu*2*pi*cos(2*pi*xI);
+% q =  diff + flow;
+% Pexp = convAdvec(h,Sig,mu,q);
+% Ptheo = sin(2*pi*xC);
+% i+1;
+% 
+% figure
+% hold on
+% plot(Pexp)
+% plot(Ptheo)
+% hold off
+% pause(10);
+% end
+% % %%this code stop running at h = 10^-5 and errors itself (ie when h is less
+% % %%than 10^-4
+end 
